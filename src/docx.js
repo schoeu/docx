c9299d3da0bfcdd2ed0c365bc71be186e865c433
liuquanyou <liuquanyou@baidu.com>
2016-08-15 23:28:36 +0800

    update run test case

--- src/docx.js
+++ src/docx.js
@@ -6,14 +6,30 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, '..', 'views'));
-app.set('view engine', 'jade');
 
-function Docx() {
-
-    
+function Docx(options) {
+    this.options = options || {};
+    this.init();
 }
+
+Docx.prototype = {
+    contributor: Docx,
+    init: function () {
+        var options = this.options;
+
+        // express 视图设置
+        app.set('views', path.join(__dirname, '..', 'views'));
+        app.set('view engine', 'jade');
+
+        app.get('/', this.render);
+
+        app.listen(options.port || 8910);
+    },
+    render: function(req, res) {
+        res.end('asdf');
+    }
+
+};
 
 
 module.exports = Docx;
---
+++ test.js
@@ -0,0 +1,5 @@
+var docx = require('./');
+
+new docx({
+    port: 8910
+});
