b4b8d4ebeecd5fc0c0d01f1a769e0ac87a98e6ad
schoeu <quanyou1013@gmail.com>
2016-08-21 17:25:44 +0800

    update docx.js

--- src/docx.js
+++ src/docx.js
@@ -148,11 +148,7 @@ Docx.prototype = {
                 htmlStr += '<li class="nav nav-title" data-path="' + item.path + '"><a href="' + item.path + '" data-pjax="true"><i class="iconfont">&#xe61a;</i><span class="nav-filename">' + item.title + '</span></a></li>';
             }
             else if (item.type === 'dir') {
-<<<<<<< HEAD
-                htmlStr += '<li data-path="' + item.path + '" class="nav nav-dir"><div class="nav-name link"><i class="iconfont">&#xe61f;</i><span class="nav-disname">' + item.displayName + '</span></div><ul class="docx-submenu">';
-=======
                 htmlStr += '<li data-dir="' + item.path + '" class="nav nav-dir"><div class="nav-name link"><i class="iconfont">&#xe61f;</i><span class="nav-disname">' + item.displayName + '</span></div><ul class="docx-submenu">';
->>>>>>> newer
                 this.makeNav(item.child);
                 htmlStr += '</ul></li>';
             }
