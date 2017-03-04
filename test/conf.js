1bdf520e0648ff142adcfc431a4d385b3f1eb06b
schoeu <quanyou1013@gmail.com>
2017-03-04 23:31:08 +0800

    add test module

---
+++ test/conf.js
@@ -0,0 +1,16 @@
+/**
+ * @file conf.js
+ * @description 配置测试文件
+ * @author schoeu
+ * */
+
+var expect = require('chai').expect;
+var config = require('../src/config');
+var confPath = './map.test.json';
+config.init(confPath);
+
+describe('config test', function () {
+    it('get config file.', function () {
+        expect(config.get('port')).to.be.equal('8910');
+    });
+});
