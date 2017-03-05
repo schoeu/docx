873568c2ac3cf4439aba18f0fb91b3cad35bc4a9
schoeu <quanyou1013@gmail.com>
2017-03-05 16:27:45 +0800

    add test module

--- test/api.js
+++ test/api.js
@@ -14,16 +14,4 @@ describe('api test.', function () {
     it('open web service.', function () {
         new Docx();
     });
-    it('get function', function () {
-        expect(config.get('headText')).to.be.equal('header');
-    });
-    it('set function', function () {
-        config.set('theme', 'newsytle');
-        expect(config.get('theme')).to.be.equal('newsytle');
-    });
-    it('refresh function', function () {
-        config.set('port', '8911');
-        config.refresh();
-        expect(config.get('port')).to.be.equal('8910');
-    });
 });
