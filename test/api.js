d54651717d597df1701f3fd3d38301a6fb80d056
schoeu <schoeu89@gmail.com>
2017-12-26 11:04:01 +0800

    update api testcase.

--- test/api.js
+++ test/api.js
@@ -8,10 +8,10 @@ var expect = require('chai').expect;
 var config = require('../src/config');
 var confPath = './test/map.test.json';
 config.init(confPath);
-var Docx = require('../src/index');
+var docx = require('../src/index');
 
 describe('api test.', function () {
     it('open web service.', function () {
-        new Docx();
+        expect(typeof docx.use).to.be.equal('function');
     });
 });
